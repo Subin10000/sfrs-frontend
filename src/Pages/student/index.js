646598548bb7b7a1279344e0af564e35de8026dc
Suyashaa <suyashaa.vaidya@wolfmatrix.com>
2023-10-07 00:03:11 +0545

    SFRS-2 Student list page

--- src/Pages/student/index.js
+++ src/Pages/student/index.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Student = () => {
-  return (
-    <div>Student</div>
-  )
-}
+  const [students, setStudents] = useState([]);
 
-export default Student
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/students");
+        const dataFromBackend = await response.json();
+
+        setStudents(dataFromBackend);
+      } catch (error) {
+        console.error("Error fetching student data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}
+      >
+        <h2 style={{ flex: 1 }}>Student Data</h2>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/addStudents"
+        >
+          Add Student
+        </Button>
+      </div>
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Roll No</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Class</TableCell>
+              <TableCell>Faculty</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{student.firstName}</TableCell>
+                <TableCell>{student.lastName}</TableCell>
+                <TableCell>{student.rollNo}</TableCell>
+                <TableCell>{student.email}</TableCell>
+                <TableCell>{student.className}</TableCell>
+                <TableCell>{student.faculty}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Student;
