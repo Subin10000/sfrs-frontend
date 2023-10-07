aa4b0d678fa638f685a34fa10535a8ab7ab22570
Sabin <sabin.sunar@wolfmatrix.com>
2023-10-07 19:54:41 +0545

    conflict resolvd

--- src/Pages/teacher/component/AddTeacher.js
+++ src/Pages/teacher/component/AddTeacher.js
@@ -25,12 +25,181 @@ const AddTeacher = () => {
   const [classList, setClassList] = useState([]);
   const [facultyList, setFacultyList] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/dropdown");
+      const apiData = response.data;
+      const semesters = apiData.filter((item) => item.type === "semester");
+      const faculties = apiData.filter((item) => item.type === "Faculty");
+      setClassList(semesters);
+      setFacultyList(faculties);
+    } catch (error) {
+      console.error("Error fetching teacher data:", error);
+    }
+  };
   useEffect(() => {
-    // Fetch class and faculty data from API and setClassList/setFacultyList
-    // Example:
-    // axios.get('/api/classes').then(response => setClassList(response.data));
-    // axios.get('/api/faculties').then(response => setFacultyList(response.data));
-  }, []); // Empty dependency array ensures this effect runs once after initial render
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // const response = await axios.post("http://localhost:8000/teacher/create");
+    // const apiData = response.data;
+  };
+
+  return (
+    <Container style={{ marginTop: "2rem", textAlign: "center" }}>
+      <Paper elevation={3} style={{ padding: 20 }}>
+        <Typography variant="h5" component="h2">
+          Add Teacher
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth style={{ minWidth: "120px" }}>
+                <InputLabel htmlFor="role-select">Role</InputLabel>
+                <Select
+                  id="role-select"
+                  name="role"
+                  value={formData.role}
+                  onChange={handleInputChange}
+                  required
+                >
+                  <MenuItem value="">Select Role</MenuItem>
+                  <MenuItem value="admin">Admin</MenuItem>
+                  <MenuItem value="teacher">Teacher</MenuItem>
+                  {/* Add other roles as MenuItems */}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth style={{ minWidth: "120px" }}>
+                <InputLabel htmlFor="class-select">Class</InputLabel>
+                <Select
+                  id="class-select"
+                  name="class"
+                  value={formData.class}
+                  onChange={handleInputChange}
+                  required
+                >
+                  <MenuItem value="">Select Class</MenuItem>
+                  {classList.map((classItem) => (
+                    <MenuItem key={classItem.id} value={classItem.id}>
+                      {classItem.title}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth style={{ minWidth: "120px" }}>
+                <InputLabel htmlFor="faculty-select">Faculty</InputLabel>
+                <Select
+                  id="faculty-select"
+                  name="faculty"
+                  value={formData.faculty}
+                  onChange={handleInputChange}
+                  required
+                >
+                  <MenuItem value="">Select Faculty</MenuItem>
+                  {facultyList.map((facultyItem) => (
+                    <MenuItem key={facultyItem.id} value={facultyItem.id}>
+                      {facultyItem.title}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                style={{ marginTop: "2rem" }}
+              >
+                Add Teacher
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default AddTeacher;
+import React, { useState, useEffect } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Container,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Grid,
+  Paper,
+} from "@mui/material";
+import axios from "axios";
+
+const AddTeacher = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: "",
+    class: "",
+    faculty: "",
+  });
+
+  const [classList, setClassList] = useState([]);
+  const [facultyList, setFacultyList] = useState([]);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/dropdown");
+      const apiData = response.data;
+      const semesters = apiData.filter(item => item.type === "semester");
+      const faculties = apiData.filter(item => item.type === "Faculty");
+      setClassList(semesters);
+      setFacultyList(faculties);
+    } catch (error) {
+      console.error("Error fetching teacher data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []); 
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -98,7 +267,7 @@ const AddTeacher = () => {
                 <Select
                   id="class-select"
                   name="selectedClass"
-                  value={formData.selectedClass}
+                  value={formData.class}
                   onChange={handleInputChange}
                   required
                 >
@@ -117,8 +286,8 @@ const AddTeacher = () => {
                 <Select
                   id="faculty-select"
                   name="selectedFaculty"
-                  value={formData.selectedFaculty}
-                  onChange={handleInputChange}
+                  value={formData.faculty}
+                  onChange={handleInputChange}  
                   required
                 >
                   <MenuItem value="">Select Faculty</MenuItem>
